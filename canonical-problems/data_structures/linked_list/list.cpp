d07ee0065aba370fddc7550d9a782f64b446c8c1
Syed Paymaan Raza <paymaan.syed@gmail.com>
2017-11-02 23:50:05 +0000

    initial data structures directory

---
+++ canonical-problems/data_structures/linked_list/list.cpp
@@ -0,0 +1,11 @@
+/// This is just a prototype and is not meant for prodduction use
+/// I'm just playing around and creating some common data structures
+
+#include <iostream>
+
+class ListNode {
+public:
+  ListNode(int x) : value(x) {}
+  int value;
+  ListNode* next;
+};
