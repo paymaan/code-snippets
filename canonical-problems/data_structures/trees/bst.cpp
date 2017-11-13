1c1ce9563b1524f06af3bc1a646ab2240e747e89
Syed Paymaan Raza <paymaan.syed@gmail.com>
2017-11-13 05:14:31 +0000

    bst part 2

--- canonical-problems/data_structures/trees/bst.cpp
+++ canonical-problems/data_structures/trees/bst.cpp
@@ -22,6 +22,11 @@ class Node {
     shared_ptr<Node> right;
 };
 
+/// BST implementation
+/// All operations are O(h) unless stated otherwise
+/// h is the height of the tree
+/// h is "n" in the worst case but
+/// "log(n)' on average.
 class BinarySearchTree {
   public:
     BinarySearchTree()
@@ -46,6 +51,43 @@ class BinarySearchTree {
 
     shared_ptr<Node> find_max() const {
         return find_max_helper(m_root);
+    }
+
+    // Value based, finds next large "val" in the tree
+    // Note that a key of provided val must be in the tree
+    // e.g. for 14 15 15 15 17 with key = 15, this would
+    // return 17
+    // Otherwise this returns nullptr
+    shared_ptr<Node> find_next_larger(const int val) const {
+        if (const auto n = search(val))
+            return n->right;
+        return nullptr;
+    }
+
+    // Similar to find_next_larger.. also value based
+    // e.g. for 14 15 15 15 17 with key = 15, this would
+    // return 14
+    shared_ptr<Node>
+    find_prev_smaller(const int val) const {
+        if (auto n = search(val)) {
+            // Our invariant (based on insertion) is:
+            // left sub tree has <= (less equal) elements
+            // right sub tree has > (greater) elements
+            // so to avoid duplicates, we go further left if
+            // needed.. because of this, we also need
+            // to go right in the left subtree to find
+            // largest smallest element
+            // 1. Go all the way to the left
+            while (n && n->left && n->left->key == val)
+                n = n->left;
+            // 2. Go all the way right in the left subtree
+            if ((n = n->left)) {
+                while (n->right && n->right->key < val)
+                    n = n->right;
+            }
+            return n;
+        }
+        return nullptr;
     }
 
   private:
@@ -96,20 +138,31 @@ class BinarySearchTree {
 
 int main() {
     // Output:
-    // found: 17
+    // found: 27
     // min: 2
     // max: 25
-  
+    // Next larger to 15: 17
+    // Prev smaller to 15: 14
+
     BinarySearchTree bst;
     bst.insert(5);
+    bst.insert(5);
+    bst.insert(15);
     bst.insert(15);
     bst.insert(2);
     bst.insert(7);
+    bst.insert(14);
+    bst.insert(15);
     bst.insert(17);
+    bst.insert(15);
     bst.insert(35);
     bst.insert(27);
+    bst.insert(2);
+    bst.insert(35);
+    bst.insert(14);
+    bst.insert(27);
 
-    if (const auto n = bst.search(17))
+    if (const auto n = bst.search(27))
         cout << "found: " << n->key << endl;
 
     if (const auto n = bst.find_min())
@@ -117,6 +170,12 @@ int main() {
 
     if (const auto n = bst.find_max())
         cout << "max: " << n->key << endl;
+
+    if (const auto n = bst.find_next_larger(15))
+        cout << "Next larger to 15: " << n->key << endl;
+
+    if (const auto n = bst.find_prev_smaller(15))
+        cout << "Prev smaller to 15: " << n->key << endl;
 
     return 0;
 }
