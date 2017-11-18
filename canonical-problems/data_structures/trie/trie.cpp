32fb3896a4a703d4c01704ae27a2dbaa1021d0c7
Syed Paymaan Raza <paymaan.syed@gmail.com>
2017-11-18 18:58:36 +0000

    trie initial setup

---
+++ canonical-problems/data_structures/trie/trie.cpp
@@ -0,0 +1,20 @@
+/// This is just a prototype and is not meant for
+/// production use
+/// I'm just playing around and creating some common data
+/// structures
+
+#include <iostream>
+#include <memory>
+
+using namespace std; // not recommended obviously but saves
+                     // some keystrokes for now :)
+
+class Trie {
+  public:
+  private:
+};
+
+int main() {
+    Trie trie;
+    return 0;
+}
